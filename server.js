afbf81d09ae5cd32347de8c5577d46847b372dbe
Erick Valencia <evalencia0911@gmail.com>
2024-01-03 16:56:36 -0700

    moved models into models folder

--- server.js
+++ server.js
@@ -7,44 +7,8 @@ const morgan = require("morgan") // our logger
 const methodOverride = require("method-override") // override forms
 const mongoose = require("mongoose") // connect to our mongodb
 
-
-//-------------------------------------
-//database connection string
-//-------------------------------------
-
-//process is a sequence of telling the processor to do something
-//an object is created that tells you everything about it
-const DATABASE_URL = process.env.DATABASE_URL
-
-//establish connection to db
-mongoose.connect(DATABASE_URL)
-
-//events for when the connection changes
-mongoose.connection
-.on("open", () => {console.log("Connected to Mongo")})
-.on("close", () => {console.log("Disconnected from Mongo")})
-.on("error", (error) => {console.log(error)})
-
-
-
-//-------------------------------------
-//Create out Fruits model
-//-------------------------------------
-//destructure schema and model into their own variables
-//takes a look for a property in the object in mongoose
-const {Schema, model} = mongoose 
-
-//Schema - shape of the data
-const fruitSchema = new Schema({
-    name: String, 
-    color: String,
-    readyToEat: Boolean
-})
-
-//Model - object for interacting with the database
-const Fruit = model("Fruit", fruitSchema)
-
-
+//import model
+const Fruit = require("./models/Fruit")
 
 //-------------------------------------
 //Express app object
